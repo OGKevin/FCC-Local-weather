ac07b3bf4080c59a274b1b0ec80e7e04cb2c8df5
OGKevin <kevinhellemun@gmail.com>
2017-03-08 21:35:51 +0100

    initiaal commit

---
+++ README.md
@@ -0,0 +1,18 @@
+##Show the Local Weather
+
+A FCC project (https://www.freecodecamp.com/challenges/show-the-local-weather)
+
+As explained on the FCC website the user should be able to do the following:
+
+* I can see the weather in my current location
+* I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather
+* I can push a button to toggle between Fahrenheit and Celsius
+
+##Know Issues
+
+* Temperature is not centered
+* There is no way to know if you are viewing Celsius or Farenheit 
+
+##Link
+
+(https://ogkevin.github.io/FCC-Local-weather)
---
+++ master.js
@@ -0,0 +1,150 @@
+$(function() {
+    var i = 0;
+    var options = {
+        url: "./city-list.json",
+        getValue: "name",
+        ajaxSettings: {
+            async: true,
+            cache: true
+        },
+        requestDelay: 500,
+        list: {
+            showAnimation: {
+                type: "fade",
+                //normal|slide|fade
+                time: 400,
+                callback: function() {}
+            },
+            hideAnimation: {
+                type: "slide",
+                //normal|slide|fade
+                time: 400,
+                callback: function() {}
+            },
+            maxNumberOfElements: 5,
+            match: {
+                enabled: true
+            }
+        },
+        theme: "plate-dark"
+    };
+    $("#locationName").easyAutocomplete(options);
+    $("#search").click(function(event) {
+        /* Act on the event */
+        i = 0;
+        locationName = $("#locationName").val();
+        getWeather(locationName);
+        console.log("search click");
+    });
+    $("#convert").click(function(event) {
+        i++;
+        if (i % 2 !== 0) {
+            temp = $("#temp").text();
+            $("#temp").html(convertTemp(temp, "farenheit"));
+            console.log(convertTemp(temp));
+        } else {
+            temp = $("#temp").text();
+            $("#temp").html(convertTemp(temp, "celsius"));
+        }
+    });
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://freegeoip.net/json/",
+        "method": "GET",
+        "headers": {
+            "cache-control": "no-cache",
+            "postman-token": "1de52cac-faa6-8480-1f9e-8d1e98a4950d"
+        }
+    }
+
+    $.ajax(settings).done(function(response) {
+        coordinates = {
+            lat: response.latitude,
+            lon: response.longitude
+        }
+        callApi(coordinates, "ip")
+        console.log(response);
+    });
+});
+
+function getWeather(name) {
+    $.ajax({
+        url: "./city-list.json",
+        async: true,
+        cache: true
+    }).done(function(data) {
+        json = JSON.parse(data);
+        // this loop is slowing it down ....
+        for (var i = 0; i < json.length; i++) {
+            if (json[i].name.toLowerCase() === name.toLowerCase()) {
+                console.log(json[i]["_id"]);
+                callApi(json[i]["_id"], "id");
+                break;
+            } else if (i === json.length - 1 && json[i].name.toLowerCase() !== name.toLowerCase()) {
+                console.log("name not found");
+            }
+        }
+    }).fail(function() {}).always(function() {});
+}
+
+function callApi(identifier, check) {
+    if (check === "id") {
+
+
+        settings = {
+            async: true,
+            crossDomain: true,
+            url: "http://api.openweathermap.org/data/2.5/weather?id=" + identifier + "&APPID=dbf0eb41d5b7b7996c2105fd1396b8cf&units=imperial",
+            method: "GET",
+            headers: {
+                "cache-control": "no-cache",
+                "postman-token": "a0d47a11-50de-8ec1-7a5f-79dc9373d5c1"
+            }
+        };
+        $.ajax(settings).done(function(response) {
+            console.log(response);
+            apllyInfo(response.name, response.main.temp, response.weather[0].description, response.weather[0].icon);
+        });
+    } else if (check === "ip") {
+        settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://api.openweathermap.org/data/2.5/weather?units=imperial&APPID=dbf0eb41d5b7b7996c2105fd1396b8cf&lat=" + identifier.lat + "&lon=" + identifier.lon,
+            "method": "GET",
+            "headers": {
+                "cache-control": "no-cache",
+                "postman-token": "1bfac9b4-76c0-c085-a55f-61410a83a48d"
+            }
+        }
+
+        $.ajax(settings).done(function(response) {
+            apllyInfo(response.name, response.main.temp, response.weather[0].description, response.weather[0].icon);
+            console.log(response);
+        });
+
+    }
+}
+
+function apllyInfo(name, temp, descr, icon) {
+    $("#name").html(name);
+    $("#temp").html(Math.round(temp));
+    $("#condition").html(descr);
+    $("#icon, #icon2").attr({
+        src: "http://openweathermap.org/img/w/" + icon + ".png"
+    });
+    // $("#info").css({
+    //     visibility: "visible"
+    // });
+}
+
+function convertTemp(temp, mode) {
+    if (mode === "farenheit") {
+        celsius = Math.round((temp - 32) * 5 / 9);
+        return celsius;
+    } else {
+        farenheit = Math.round(temp * 9 / 5 + 32);
+        return farenheit;
+    }
+}
